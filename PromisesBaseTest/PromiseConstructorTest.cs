8dcd38373f1689271137a24b5fa0d94cf4f183b8
Joe Termine <joe@termine.com>
2014-11-11 11:45:42 -0500

    Moved the folders around

---
+++ PromisesBaseTest/PromiseConstructorTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Termine.Promises;
+
+namespace PromisesBaseTest
+{
+    [TestClass]
+    public class PromiseConstructorTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var promise = new Promise().WithAuthChallenger(f => { return; }).RunAsync();
+            Assert.IsNotNull(promise);
+        }
+    }
+}
