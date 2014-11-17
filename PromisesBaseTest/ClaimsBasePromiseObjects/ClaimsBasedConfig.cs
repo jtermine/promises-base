9244ba0ff67f327d9c40159548f812368ffbe8ca
Joe Termine <joe@termine.com>
2014-11-17 10:03:41 -0500

    Committing the nuget package tree
    So that all of the build processes have the appropriate package

---
+++ PromisesBaseTest/ClaimsBasePromiseObjects/ClaimsBasedConfig.cs
@@ -0,0 +1,18 @@
+﻿using Termine.Promises.ClaimsBasedAuth.Base.Interfaces;
+
+namespace Termine.Promises.Base.Test.ClaimsBasePromiseObjects
+{
+    public class ClaimsBasedConfig: IConfigureClaims
+    {
+        public string HmacSigningKey { get; private set; }
+        public string HmacAudienceUri { get; private set; }
+        public string HmacIssuer { get; private set; }
+
+        public ClaimsBasedConfig()
+        {
+            HmacSigningKey = "1";
+            HmacAudienceUri = "audience";
+            HmacIssuer = "issuer";
+        }
+    }
+}
