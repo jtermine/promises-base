e8b2e8730c3c87300fcb36366ee3c98abdf5c8a1
Joe Termine <joe@termine.com>
2014-12-01 00:50:46 -0500

    Added bytearray support to protobuf serialization

--- PromisesWithProtobuf/Extensions.cs
+++ PromisesWithProtobuf/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using ProtoBuf;
 using Termine.Promises.WithProtobuf.Interfaces;
 
@@ -15,6 +16,26 @@ namespace Termine.Promises
         public static TT Deserialize<TT>(this Stream inputStream) where TT : class, ISupportProtobuf
         {
             return Serializer.Deserialize<TT>(inputStream);
+        }
+
+        public static Byte[] ToByteArray<TT>(this TT model) where TT : class, ISupportProtobuf
+        {
+            var memoryStream = new MemoryStream();
+            
+            Serializer.Serialize(memoryStream, model);
+
+            memoryStream.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream.ToArray();
+        }
+
+        public static TT FromByteArray<TT>(this byte[] modelBytes) where TT : class, ISupportProtobuf
+        {
+            using (var memoryStream = new MemoryStream(modelBytes))
+            {
+                return Serializer.Deserialize<TT>(memoryStream);
+            }
         }
     }
 }
