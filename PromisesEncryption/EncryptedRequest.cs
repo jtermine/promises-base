d66b7611d4354b636e642d4bf5248c80417f8bc4
Joe Termine <joe@termine.com>
2014-12-18 08:23:51 -0500

    added promises encrption library

---
+++ PromisesEncryption/EncryptedRequest.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Termine.Promises
+{
+    public class EncryptedRequest
+    {
+        public byte[] PayloadBytes { get; set; }
+        public byte[] NonceBytes { get; set; }
+
+        public EncryptedRequest ()
+        {
+            
+        }
+
+        public EncryptedRequest(byte[] payloadBytes, byte[] nonceBytes)
+        {
+            PayloadBytes = payloadBytes;
+            NonceBytes = nonceBytes;
+        }
+    }
+}
