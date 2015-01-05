0c3e78f0ec14d7712621393047a6cd42b373f9e2
Joe Termine <joe@termine.com>
2015-01-04 23:18:21 -0500

    Configures projects to generate XML documentation

--- PromisesEncryption/Extensions.cs
+++ PromisesEncryption/Extensions.cs
@@ -4,6 +4,12 @@ namespace Termine.Promises.Encryption
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Encrypts a stream of bytes using a secret key and boxes this into an EncryptedRequest
+        /// </summary>
+        /// <param name="message">the stream of bytes to encrypt</param>
+        /// <param name="key">the secret key to use</param>
+        /// <returns>an EncryptedRequest that contains the message, the encrypted payload, a nonce, and the secret key</returns>
         public static EncryptedRequest Encrypt(this byte[] message, byte[] key)
         {
             var nonce = StreamEncryption.GenerateNonce();
@@ -13,6 +19,12 @@ namespace Termine.Promises.Encryption
             return new EncryptedRequest(message, payloadBytes, nonce, key);
         }
 
+        /// <summary>
+        /// Decrypts the byte array in an EncryptedRequest.EncryptedPayloadBytes using a given secret key and nonce
+        /// </summary>
+        /// <param name="request">the EncryptedRequest that has the property EncryptedPayloadBytes and NonceBytes populated</param>
+        /// <param name="key">the secret key used to decrypt the bytes</param>
+        /// <returns>a an array of bytes that is the decrypted message</returns>
         public static byte[] Decrypt(this EncryptedRequest request, byte[] key)
         {
             var response = StreamEncryption.Decrypt(request.EncryptedPayloadBytes, request.NonceBytes, key);
